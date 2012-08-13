35a172df34274f75693c0f180be419d96049f71d
Viktor <vila@vila>
2012-08-13 11:48:50 +0200

    different color support

--- draw_perimeter.m
+++ draw_perimeter.m
@@ -1,14 +1,25 @@
-function f_perim = draw_perimeter(f, C)
+function f_perim = draw_perimeter(f, C, c)
 %DRAW_PERIMETER Given a grayscale image and a characteristic function
 % returns an image with the perimeter of C drawn overlayed on f
+% with color c
+
+if nargin < 3
+    c = [1 0 0];
+end
 
 % find the perimeter of C
 perim = bwperim(C);
 
 % we dilate the perimiter to make it a little bit thicker border
 perim = imdilate( perim, strel('square',2) );
-       
+
+if (size(f,3) < 3)
+   f = repmat(f, [1 1 3]); 
+end
+
+[m,n,~] = size(f);
+
 % create a new image with the border overlayed
-f_perim = repmat(f.*~perim, [1 1 3]) + cat(3, perim,zeros([size(f) 2]));
+f_perim = f .* repmat(~perim, [1 1 3]) + cat(3, c(1)*perim, cat(3, c(2)*perim, c(3)*perim));
 
 end
