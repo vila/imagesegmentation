2fc632fd7f014c1c57e63e6678bbae0ec5edb887
Viktor <vila@vila>
2012-08-13 11:47:29 +0200

    grad and div now wraps around

--- div.m
+++ div.m
@@ -2,7 +2,12 @@ function div_A = div(A)
 %DIV Calculates the divergence as specified in the paper by Chambolle
 % A should be MxNx2
 
-A1 = [A(:,1,1) (A(:,2:end-1,1) - A(:,1:end-2,1)) -A(:,end-1,1)];
-A2 = [A(1,:,2);(A(2:end-1,:,2) - A(1:end-2,:,2)); -A(end-1,:,2)];
+% chambolles definition
+%A1 = [A(:,1,1) (A(:,2:end-1,1) - A(:,1:end-2,1)) -A(:,end-1,1)];
+%A2 = [A(1,:,2);(A(2:end-1,:,2) - A(1:end-2,:,2)); -A(end-1,:,2)];
+
+% circular definition
+A1 = A(:,:,1) - A(:,[end 1:end-1], 1);
+A2 = A(:,:,2) - A([end 1:end-1],:, 2);
 
 div_A = A1 + A2;
--- grad.m
+++ grad.m
@@ -2,8 +2,13 @@ function grad_A = grad(A)
 %GRAD Computes the gradient as specified in the paper by Chambolle
 % A should be a MxN matrix
 
-A1 = [(A(:,2:end)-A(:,1:end-1)) zeros(size(A,1),1)];
-A2 = [(A(2:end,:)-A(1:end-1,:)); zeros(1,size(A,2))];
+% chambolles definition
+%A1 = [(A(:,2:end)-A(:,1:end-1)) zeros(size(A,1),1)];
+%A2 = [(A(2:end,:)-A(1:end-1,:)); zeros(1,size(A,2))];
+
+% circular definition
+A1 = A(:,[2:end 1]) - A;
+A2 = A([2:end 1],:) - A;
 
 grad_A(:,:,1) = A1;
 grad_A(:,:,2) = A2;
